0402d74d5b8703f2b5d8cacaabbc6783c6be20b7
Belyy Roman <RMBelyy.SBT@sberbank.ru>
2016-09-09 16:05:00 +0300

    init project

---
+++ README.md
@@ -0,0 +1,38 @@
+# wercker-bitbucket-upload
+
+A wercker bitbucket uploader written in `bash` and `curl`.
+
+[![wercker status](https://app.wercker.com/status/7ed9584e773844e30cbd65b729595a14/m "wercker status")](https://app.wercker.com/project/bykey/7ed9584e773844e30cbd65b729595a14)
+
+# Options
+
+- `key` OAuth key
+- `secret` OAuth secret
+- `file` filename path
+- 'git_owner: "AVTopchiy"
+              git_repository: "km-cib-stubs"
+
+OAuth needs a key and secret, together these are know as an OAuth consumer. You can create a consumer on any existing individual or team account. 
+
+# Example
+
+```yaml
+build:
+    steps:
+        - phantomx/bitbucket-upload-artifact@0.0.1:
+            key: g25sMaaBypPR4QccyH
+            secret: dh87Krw9bwSFHMdr2mvw552LqNJdq
+            file: war/out.jar
+            git_owner: phantomx
+            git_repository: bitbucket-upload
+```
+
+# License
+
+The MIT License (MIT)
+
+# Changelog
+
+## 0.0.1
+
+- Initial release
---
+++ run.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+if [ ! -n "$WERCKER_BITBUCKET_UPLOAD_ARTIFACT_KEY" ]; then
+  error 'Please specify key property'
+  exit 1
+fi
+
+if [ ! -n "$WERCKER_BITBUCKET_UPLOAD_ARTIFACT_SECRET" ]; then
+  error 'Please specify secret property'
+  exit 1
+fi
+
+if [ ! -n "$WERCKER_BITBUCKET_UPLOAD_ARTIFACT_FILE" ]; then
+  error 'Please specify file property'
+  exit 1
+fi
+
+if [ ! -n "$WERCKER_BITBUCKET_UPLOAD_ARTIFACT_GIT_OWNER" ]; then
+  error 'Please specify git_owner property'
+  exit 1
+fi
+
+if [ ! -n "$WERCKER_BITBUCKET_UPLOAD_ARTIFACT_GIT_REPOSITORY" ]; then
+  error 'Please specify git_repository property'
+  exit 1
+fi
+
+pge=$WERCKER_BITBUCKET_UPLOAD_ARTIFACT_GIT_OWNER/$WERCKER_BITBUCKET_UPLOAD_ARTIFACT_GIT_REPOSITORY/downloads
+
+urlDownload=https://api.bitbucket.org/2.0/repositories/$pge
+
+secret_key=$WERCKER_BITBUCKET_UPLOAD_ARTIFACT_KEY:$WERCKER_BITBUCKET_UPLOAD_ARTIFACT_SECRET
+
+response=$(curl -X POST -u "$secret_key" \
+ https://bitbucket.org/site/oauth2/access_token \
+  -d grant_type=client_credentials)
+
+echo "$response"
+regex="\"(access_token)\": \"([^\"]*)\""
+
+if [[ $response =~ $regex ]]
+then
+    access_token="${BASH_REMATCH[2]}"
+    echo "$access_token"
+else
+    error "$f doesn't match" >&2 # this could get noisy if there are a lot of non-matching files
+    exit 1
+fi
+
+curl -v \
+  --header "Authorization: Bearer $access_token" \
+  --form "files=@$WERCKER_BITBUCKET_UPLOAD_ARTIFACT_FILE" \
+  --output ./utbb \
+  "https://api.bitbucket.org/2.0/repositories/$pge"
+
+if [ -f ./utbb ] && [ ! -z "$(grep error ./utbb)" ]; then
+    error $(cat ./utbb)
+    exit 1
+fi
+
